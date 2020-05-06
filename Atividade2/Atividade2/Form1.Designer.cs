f93e063df7245e41bb4d481aaf9104060b82204f
Matheus de Almeida Oliveira <9852matheus@gmail.com>
2020-05-06 15:54:00 -0300

    Atividade2

---
+++ Atividade2/Atividade2/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Atividade2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbtMasculino = new System.Windows.Forms.RadioButton();
+            this.rbtFeminino = new System.Windows.Forms.RadioButton();
+            this.mtxPeso = new System.Windows.Forms.MaskedTextBox();
+            this.mtxAltura = new System.Windows.Forms.MaskedTextBox();
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbtMasculino);
+            this.groupBox1.Controls.Add(this.rbtFeminino);
+            this.groupBox1.Location = new System.Drawing.Point(64, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(124, 94);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sexos";
+            // 
+            // rbtMasculino
+            // 
+            this.rbtMasculino.AutoSize = true;
+            this.rbtMasculino.Location = new System.Drawing.Point(23, 55);
+            this.rbtMasculino.Name = "rbtMasculino";
+            this.rbtMasculino.Size = new System.Drawing.Size(73, 17);
+            this.rbtMasculino.TabIndex = 1;
+            this.rbtMasculino.TabStop = true;
+            this.rbtMasculino.Text = "Masculino";
+            this.rbtMasculino.UseVisualStyleBackColor = true;
+            // 
+            // rbtFeminino
+            // 
+            this.rbtFeminino.AutoSize = true;
+            this.rbtFeminino.Location = new System.Drawing.Point(23, 32);
+            this.rbtFeminino.Name = "rbtFeminino";
+            this.rbtFeminino.Size = new System.Drawing.Size(67, 17);
+            this.rbtFeminino.TabIndex = 0;
+            this.rbtFeminino.TabStop = true;
+            this.rbtFeminino.Text = "Feminino";
+            this.rbtFeminino.UseVisualStyleBackColor = true;
+            // 
+            // mtxPeso
+            // 
+            this.mtxPeso.Location = new System.Drawing.Point(102, 186);
+            this.mtxPeso.Name = "mtxPeso";
+            this.mtxPeso.Size = new System.Drawing.Size(100, 20);
+            this.mtxPeso.TabIndex = 1;
+            // 
+            // mtxAltura
+            // 
+            this.mtxAltura.Location = new System.Drawing.Point(102, 139);
+            this.mtxAltura.Name = "mtxAltura";
+            this.mtxAltura.Size = new System.Drawing.Size(100, 20);
+            this.mtxAltura.TabIndex = 2;
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.Location = new System.Drawing.Point(87, 233);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(75, 23);
+            this.btnExecutar.TabIndex = 3;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = true;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Altura";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Peso(Kg)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 282);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExecutar);
+            this.Controls.Add(this.mtxAltura);
+            this.Controls.Add(this.mtxPeso);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbtMasculino;
+        private System.Windows.Forms.RadioButton rbtFeminino;
+        private System.Windows.Forms.MaskedTextBox mtxPeso;
+        private System.Windows.Forms.MaskedTextBox mtxAltura;
+        private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Atividade2/Atividade2/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Atividade2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            double altura = Convert.ToDouble(mtxAltura.Text);
+
+            if (rbtFeminino.Checked)
+            {
+                double result = 62.1 * altura - 44.7;
+                double peso = Convert.ToDouble(mtxPeso.Text);
+                if (peso > result)
+                {
+                    MessageBox.Show("Regime Obrigatório Já");
+                }
+                else if (peso < result)
+                {
+                    MessageBox.Show("Coma bastante massas e doces");
+                }
+                else
+                {
+                    MessageBox.Show("Você está com o peso ideal");
+                }
+            }
+            else if (rbtMasculino.Checked)
+            {
+                double result = 72.7 * altura - 58;
+                double peso = Convert.ToDouble(mtxPeso.Text);
+                if (peso > result)
+                {
+                    MessageBox.Show("Regime Obrigatório Já");
+                }
+                else if (peso < result)
+                {
+                    MessageBox.Show("Coma bastante massas e doces");
+                }
+                else
+                {
+                    MessageBox.Show("Você está com o peso ideal");
+                }
+            }
+        }
+    }
+}
